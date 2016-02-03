1876206cc9683fb61a1dc81c1522f9c75fc4d2ff
mahdibagheri <150mahdi@gmail.com>
2016-02-03 10:30:09 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,29 +226,14 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {77750573},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[aqa_mp3 v5.8
+    
+   id channel bot : @aqamp3
+id admin bot : @mahdimp3
+id bot : @aqa_mp3
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -314,122 +299,91 @@ Only sudo users can run this command
 This command will send text to [group_id]
 
 
-**U can use both "/" and "!" 
-
-
-*Only admins and sudo can add bots in group
-
-
-*Only admins and sudo can use kick,ban,unban,newlink,setphoto,setname,lock,unlock,set rules,set about and settings commands
-
-*Only admins and sudo can use res, setowner, commands
 ]],
     help_text = [[
 Commands list :
-
+#1 Remove 
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
 !kick [username|id]
 You can also do it by reply
-
 !ban [ username|id]
 You can also do it by reply
-
 !unban [id]
 You can also do it by reply
-
-!who
-Members list
-
-!modlist
-Moderators list
-
-!promote [username]
-Promote someone
-
-!demote [username]
-Demote someone
-
 !kickme
-Will kick user
-
-!about
-Group description
-
+remove khod az group
+!banlist
+will return group ban list
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#2 admin
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+!promote [username]
+add admin be group 
+!demote [username]
+remove admin az group
+!modlist 
+dedan admins group
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#3 Changes
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
 !setphoto
-Set and locks group photo
-
+Changes photo group
 !setname [name]
-Set group name
-
-!rules
-Group rules
-
-!id
-return group id or user id
-
-!help
-
-!lock [member|name|bots|leave]	
+Changes name group {nam gp}
+!lock [member|name|bots|leave]
 Locks [member|name|bots|leaveing] 
-
 !unlock [member|name|bots|leave]
 Unlocks [member|name|bots|leaving]
-
 !set rules <text>
 Set <text> as rules
-
 !set about <text>
 Set <text> as about
-
-!settings
-Returns group settings
-
-!newlink
-create/revoke your group link
-
-!link
-returns group link
-
-!owner
-returns group owner id
-
-!setowner [id]
-Will set id as owner
-
 !setflood [value]
 Set [value] as flood sensitivity
-
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#4 admin group 
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+!setowner [id]
+Will set id as owner
+!owner
+returns group owner id
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#5 link 
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+!link
+returns group link
+!newlink
+create/revoke your group link
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#6 .............
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+!who
+Members list
+!about
+Group description
+!rules
+Group rules
+!id
+return group id or user id
+!settings
+Returns group settings
 !stats
 Simple message statistics
-
 !save [value] <text>
 Save <text> as [value]
-
 !get [value]
 Returns text of [value]
-
 !clean [modlist|rules|about]
 Will clear [modlist|rules|about] and set it to nil
-
 !res [username]
 returns user id
 "!res @username"
-
 !log
 will return group logs
-
-!banlist
-will return group ban list
-
-**U can use both "/" and "!" 
-
-
-*Only owner and mods can add bots in group
-
-
-*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
-
-*Only owner can use res,setowner,promote,demote and log commands
-
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــ
+id channel bot : @aqamp3
+id admin bot : @mahdimp3
+id bot : @aqa_mp3
 ]]
   }
   serialize_to_file(config, './data/config.lua')
